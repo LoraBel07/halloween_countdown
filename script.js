f5fa4fef491dcb1687f56e93bf3bd0369a206de3
LoraBel07 <91973134+LoraBel07@users.noreply.github.com>
2022-10-29 22:40:55 -0400

    commit

--- script.js
+++ script.js
@@ -40,74 +40,12 @@ function happyHalloween() {
 const button = document.querySelector("#myButton");
 button.addEventListener("click", function() {
     document.querySelector("#myAudio").play();
-})
+});
 const buttonTwo = document.querySelector("#myButtonTwo");
 buttonTwo.addEventListener("click", function() {
     document.querySelector("#myAudio").pause();
-})
+});
 
-// function display_random_image() {
-//     const theImages = [{
-//         src: "1c.png",
-//         width: "300",
-//         height: "500"
-//     }, {
-//         src: "2c.png",
-//         width: "300",
-//         height: "500"
-//     }, {
-//         src: "3c.png",
-//         width: "300",
-//         height: "500"
-//     }, {
-//         src: "4c.png",
-//         width: "300",
-//         height: "500"
-//     }, {
-//         src: "5c.png",
-//         width: "300",
-//         height: "500"
-//     }, {
-//         src: "6c.png",
-//         width: "300",
-//         height: "500"
-//     }, {
-//         src: "7c.png",
-//         width: "300",
-//         height: "500"
-//     }
-// ];
-// const preBuffer = [];
-//     for (let i = 0, j = theImages.length; i < j; i++) {
-//         preBuffer[i] = new Image();
-//         preBuffer[i].src = theImages[i].src;
-//         preBuffer[i].width = theImages[i].width;
-//         preBuffer[i].height = theImages[i].height;
-//     }
-
-// // create random image number
-//     function getRandomInt(min,max) 
-//     {
-//     //  return Math.floor(Math.random() * (max - min + 1)) + min;
-    
-//     imn = Math.floor(Math.random() * (max - min + 1)) + min;
-//     return preBuffer[imn];
-//     }  
-
-// // 0 is first image,   preBuffer.length - 1) is  last image
-
-// const newImage = getRandomInt(0, preBuffer.length - 1);
-
-// // remove the previous images
-// const images = document.getElementsByTagName('img');
-// let l = images.length;
-// for (let p = 0; p < l; p++) {
-//     images[0].parentNode.removeChild(images[0]);
-// }
-// // display the image  
-
-// document.body.appendChild(newImage);
-// }
 
 const images = ['1c.png', '2c.png', '3c.png', '4c.png', '5c.png', '6c.png', '7c.png', '8c.png', '9c.png', '10c.png', '11c.png', '12c.png', '14c.png', '15c.png', '16c.png', '17c.png', '18c.png', '19c.png', '20c.png', '21c.png', '22c.png', '23c.png', '24c.png', '25c.png', '27c.png', '28c.png', '29c.png', '30c.png', '31c.png','32c.png', '33c.png', '34c.png', '36c.png', '37c.png', '38c.png', '39c.png', '40c.png', '41c.png', '42c.png', '43c.png', '44c.png', '45c.png'];
 
